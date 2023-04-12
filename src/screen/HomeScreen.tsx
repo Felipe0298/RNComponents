8a5b4a6b6e955d10a0496885be1b36cda426baae
Felipe Hoyos <felipe02-98@hotmail.com>
2023-04-12 13:58:40 -0500

    190. Componente - FlatList

--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -1,15 +1,41 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 import Icon from "react-native-vector-icons/Ionicons"
 
+interface menuItem {
+    name: string;
+    icon: string;
+    components: string;
+}
+
+const menuItems = [
+    {
+        name: "Animation 101",
+        icon: "cube-outline",
+        components: "Animation101Screen"
+    }
+]
 export const HomeScreen = () => {
+
+    const renderMenuItem = (menuItem: menuItem) => {
+        return (
+            <View>
+                <Text style={{color:"black"}}>{menuItem.name} - {menuItem.icon}</Text>
+            </View>
+        )
+    }
+
+
     return (
-        <View>
-            <Icon
-                name='star-outline'
-                size={50}
-                color="black"
+        <View style={{ flex: 1, }}>
+
+            <FlatList
+                data={menuItems}
+                renderItem={({item}) => renderMenuItem( item )}
+                keyExtractor={(item)=> item.name} //TIene que ser algo unico y un string
             />
+
         </View>
-    );
+    )
 }
