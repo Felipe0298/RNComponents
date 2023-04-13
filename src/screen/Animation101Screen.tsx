1bb151c13354968156dc678b089a02fb2fb0d6dc
Felipe Hoyos <felipe02-98@hotmail.com>
2023-04-13 12:13:57 -0500

    195. FadeIn y FadeOut

--- src/screen/Animation101Screen.tsx
+++ src/screen/Animation101Screen.tsx
@@ -1,16 +1,43 @@
 import React, { useRef } from 'react';
-import { Animated, StyleSheet, Text, View } from 'react-native';
+import { Animated, Button, StyleSheet, Text, View } from 'react-native';
 
 export const Animation101Screen = () => {
 
-    const opacity = useRef(new Animated.Value(0.4)).current;
+    const opacity = useRef(new Animated.Value(0)).current;
+
+    const fadeIn = () =>{
+        Animated.timing(
+            opacity,
+            {
+                toValue:1,
+                duration:300,
+                useNativeDriver:true //Ayuda a que la animation sea acelerada por hardware
+            }
+        ).start( () => console.log("Animacion terminó"))
+    }
+
+    const fadeOut= ()=>{
+        Animated.timing(
+            opacity,
+            {
+                toValue:0,
+                duration:300,
+                useNativeDriver:true
+            }
+        ).start()
+    }
 
 
     return (
         <View style={styles.container}>
             <Animated.View style={{...styles.purpleBox,
-            opacity
+            opacity,
+            marginBottom:20,
             }} />
+
+
+            <Button title="FadeIn" onPress={fadeIn} />
+            <Button title='FadeOut' onPress={fadeOut}/>
         </View>
     );
 }
@@ -19,11 +46,12 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
     },
     purpleBox: {
         backgroundColor: "#5856D6",
         width: 150,
         height: 150
-    }
+    },
+    
 });
