04857d186839db574f051947453bbcda05ac33e7
Felipe Hoyos <felipe02-98@hotmail.com>
2023-04-14 15:49:32 -0500

    207. Multiples TextInputs en pantalla

--- src/screen/TextInputScreen.tsx
+++ src/screen/TextInputScreen.tsx
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {StyleSheet, Text, TextInput, View} from 'react-native';
 import { HeaderTitle } from '../components/HeaderTitle';
 import { styles } from '../theme/appTheme';
 
 export const TextInputScreen = () => {
+
+    const [form, setForm] = useState({
+        name:"",
+        email:"",
+        phone:"",
+    })
+
+    const onChange = (value:string, field: string) =>{
+        setForm({
+            ...form,
+            [field]: value
+        })
+    }
+
     return (
         <View style={styles.globalMargin}>
             <HeaderTitle title='Text Inputs'/>
 
-            <TextInput style={stylesScreen.textInputStyle}/>
+            <TextInput style={stylesScreen.textInputStyle}
+                placeholder='Ingrese su nombre'
+                autoCorrect={false}
+                autoCapitalize="words"
+                onChangeText={(value) => onChange(value, "name")}
+            />
+            <TextInput style={stylesScreen.textInputStyle}
+                placeholder='Ingrese su email'
+                autoCorrect={false}
+                autoCapitalize="none"   
+                onChangeText={(value) => onChange(value, "email")}   
+               keyboardType='email-address'
+                />
+
+            <TextInput style={stylesScreen.textInputStyle}
+                placeholder='Ingrese su telefono'
+                onChangeText={(value) => onChange(value, "phone")}
+                keyboardType='phone-pad'
+            
+            />
+
+            <HeaderTitle title={JSON.stringify(form,null,3)}/>
         </View>
     );
 }
@@ -20,6 +55,7 @@ export const stylesScreen = StyleSheet.create({
         opacity:0.3,
         height:50,
         paddingHorizontal:10,
-        borderRadius:10
+        borderRadius:10,
+        marginVertical:10
     }
 });
