3f9a9c466e2bb325681cef4f963930748113d177
Felipe Hoyos <felipe02-98@hotmail.com>
2023-04-20 20:26:18 -0500

    215. Personalizar el modal

--- src/screen/ModalScreen.tsx
+++ src/screen/ModalScreen.tsx
@@ -1,37 +1,64 @@
-import React, { useState } from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
-import { styles } from '../theme/appTheme';
+import React, { useState } from 'react'
+import { Button, Modal, Text, View } from 'react-native'
 import { HeaderTitle } from '../components/HeaderTitle';
-import { Modal } from 'react-native';
+import { styles } from '../theme/appTheme';
 
 export const ModalScreen = () => {
 
-    const [isVisible, setIsVisible] = useState(false)
+    const [isVisible, setIsVisible] = useState(false);
 
     return (
-        <View style={styles.globalMargin}>
-            <HeaderTitle title='ModalScreen'/>
+        <View style={ styles.globalMargin }>
+            <HeaderTitle title="Modal Screen" />
 
-            <Button title='Abrir modal' onPress={()=> setIsVisible(true)}/>
+            <Button 
+                title="Abrir modal"
+                onPress={ () => setIsVisible(true) }
+            />
 
             <Modal
-                animationType='slide'
-                visible={isVisible}
-                transparent={true}
+                animationType="fade"
+                visible={ isVisible }
+                transparent={ true }
             >
-                                {/* Background negro */}
-                <View style={{flex:1 /* width:100, height:100 */, backgroundColor:"rgba(0,0,0,0.3)"}}/>
 
-                                {/* Contenido del modal */}
-                <View
-                style={{backgroundColor:"white"}}>
-                    <HeaderTitle title='Modal'/>
-                    <Text>Cuerpo del modal</Text>
-                    <Button title='Cerrar'
-                    onPress={()=> setIsVisible(false)}/>
+                {/* Background negro */}
+                <View style={{
+                    flex: 1,
+                    backgroundColor: 'rgba(0,0,0,0.3)',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
+
+                    {/* Contenido del modal */}
+                    <View style={{
+                        width: 200,
+                        height: 200,
+                        backgroundColor: 'white',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        shadowOffset: {
+                            width: 0,
+                            height: 10
+                        },
+                        shadowOpacity: 0.25,
+                        elevation: 10,
+                        borderRadius: 5
+                    }}>
+                        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Modal</Text>
+                        <Text style={{ fontSize: 16, fontWeight: '300', marginBottom: 20 }}>Cuerpo del modal</Text>
+                        <Button
+                            title="Cerrar"
+                            onPress={ () => setIsVisible(false) }
+                        />
+                    </View>
+
+
                 </View>
 
             </Modal>
+
+
         </View>
-    );
+    )
 }
