3cf89360ae7766a7186ab72148b7a6e192a95690
Felipe Hoyos <felipe02-98@hotmail.com>
2023-04-24 11:41:18 -0500

    217. InfiniteScroll con imágenes

--- src/screen/InfiniteScrollScreen.tsx
+++ src/screen/InfiniteScrollScreen.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, FlatList, Image, ActivityIndicator} from 'react-native';
 import { HeaderTitle } from '../components/HeaderTitle';
-import { FlatList } from 'react-native-gesture-handler';
 
 export const InfiniteScrollScreen = () => {
 
@@ -13,14 +12,21 @@ export const InfiniteScrollScreen = () => {
             newArray[i] = numbers.length + i;
         }
 
-        setNumbers([...numbers, ...newArray])
+        setTimeout(() => {
+            setNumbers([...numbers, ...newArray])
+
+        }, 1500);
     }
 
     const renderItem = (item: number) =>{
         return(
-            <Text style={styles.textItem}>
-                {item}
-            </Text>
+            <Image
+                source={{uri: `https://picsum.photos/id/${item}/500/400`}}
+                style={{
+                    width:"100%",
+                    height:400
+                }}
+            />
         )
     }
 
@@ -34,6 +40,16 @@ export const InfiniteScrollScreen = () => {
             ListHeaderComponent={ <HeaderTitle title='InfiniteScroll'/>}
             onEndReached={loadMore}
             onEndReachedThreshold={0.5}
+
+            ListFooterComponent={()=>(
+                <View style={{
+                    height:150,
+                    justifyContent:"center",
+                    alignItems:"center"
+                }}>
+                    <ActivityIndicator size={25} color="#5856D6"/>
+                </View>
+            )}
             />
         </View>
     );
